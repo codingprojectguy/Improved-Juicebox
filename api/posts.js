4fb7b2d2cd3e72e79b3e00b85189b0fb3e1e8877
ZJ <vfxprojectguy@gmail.com>
2023-11-11 11:57:08 -0800

    Build PUT /api/posts/:id add function to update a post only if it was created by the currently logged in user

--- api/posts.js
+++ api/posts.js
@@ -68,4 +68,49 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+//PUT update a post only if it was created by currently logged in user.
+router.put("/:id", async (req, res, next) => {
+  try {
+    const userId = +req.headers.authorization;
+    if (!userId) {
+      return next({
+        status: 401,
+        message: "must login first",
+      });
+    }
+    const id = +req.params.id;
+    const postExists = await prisma.post.findUnique({ where: { id } });
+    if (!postExists) {
+      return next({
+        status: 404,
+        message: `Can not find post id${id}`,
+      });
+    }
+    if (postExists.userId !== userId) {
+      return next({
+        status: 403,
+        message: "you can't update other user's post",
+      });
+    }
+
+    const { userTitle, userContent } = req.body;
+    if (!userTitle || !userContent) {
+      const error = {
+        status: 400,
+        message: "title and content must included",
+      };
+      return next(error);
+    }
+
+    const update = await prisma.post.update({
+      where: { id },
+      data: { title: userTitle, content: userContent },
+    });
+
+    res.send(update);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
